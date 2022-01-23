6002e9b768d7c098b57141a3a1d1e6ec45018d00
alexf <rassylka82@inbox.ru>
2022-01-23 11:16:47 +0300

    lesson13

---
+++ src/main/java/lesson13/MainClass.java
@@ -0,0 +1,38 @@
+package lesson13;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
+public class MainClass {
+    public static final int CARS_COUNT = 4;
+    public static String winner = "";
+    public static CountDownLatch start = new CountDownLatch(CARS_COUNT);
+    public static CountDownLatch finish = new CountDownLatch(CARS_COUNT);
+    public static CountDownLatch win = new CountDownLatch(1);
+    public static CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT);
+    public static Semaphore semaphore = new Semaphore((int) Math.ceil(CARS_COUNT/2));
+
+
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Сar[] cars = new Сar[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Сar(race, 20 + (int) (Math.random() * 10));
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+        try {
+            start.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            win.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Победил: " + winner);
+            finish.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/lesson13/Сar.java
@@ -0,0 +1,46 @@
+package lesson13;
+
+public class Сar implements Runnable {
+    private static int CARS_COUNT;
+    static {
+        CARS_COUNT = 0;
+    }
+    private final Race race;
+    private final int speed;
+    private final String name;
+
+    public String getName() {
+        return name;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+
+
+    public Сar(Race race, int speed) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+    }
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int)(Math.random() * 800));
+            System.out.println(this.name + " готов");
+            MainClass.start.countDown();
+            MainClass.cyclicBarrier.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+        if (MainClass.winner.equals("")) {
+            MainClass.winner = this.name;
+        }
+        MainClass.win.countDown();
+        MainClass.finish.countDown();
+    }
+}
